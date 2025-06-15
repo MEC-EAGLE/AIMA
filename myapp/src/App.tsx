ed0c2a0e44dd190bb579a6568963c47891f4cfce
MEC-EAGLE <168678922+MEC-EAGLE@users.noreply.github.com>
2025-06-15 16:25:48 -0400

    format router import

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
